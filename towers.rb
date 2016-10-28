27e526874298c760dc0ee153dc2ed3a16eae58b2
Jessica G <gillan.jessica@gmail.com>
2016-10-28 16:18:23 -0600

    add comments, small refactoring, script input with ARGV

--- towers.rb
+++ towers.rb
@@ -1,15 +1,31 @@
+# !/usr/bin/ruby
+
+# Play Tower of Hanoi
+# Start with a stack of disks of varying size, stacked largest to smallest
+# and move it to another postion (3 possible possitions) by moving one
+# disk at a time, always grabbing the top disk, and never stacking a disk 
+# on top of a disk smaller than it.
+
+# Option to enter number of disks as script input, default is 3.
+
 class TowersOfHanoi
 
-	def initialize(num_disks = 3)
-		@disks = num_disks		
+	def initialize(num_disks)
+		@disks = num_disks	
 	end
+
+	# Create the starting stack
 
 	def full_stack
 		return (1..@disks).to_a.reverse
 	end
 
+	# Main method
+
 	def play
 		loop do
+			# Initialize the disks
+
 			@stacks = [ full_stack, [], [] ]	
 
 			puts "---------------------------------------------------"
@@ -35,25 +51,30 @@ class TowersOfHanoi
 		end 
 	end
 
+	# Display the current state of disks
+
 	def render
 
+		# Display the disks from top to bottom, row by row
 		@disks.times do |i|
 			@stacks.length.times do |j|
 
-				if @stacks[j][ @disks - i - 1 ].nil?
+				if @stacks[j][ @disks - i - 1 ].nil? # Empty position
 					print " ".center(11)
 				else
-					print ('#' * @stacks[j][ @disks - i - 1 ]).center(11)
+					print ('#' * @stacks[j][ @disks - i - 1 ]).center(11) # Print the disk, sized with a number of '#'s
 				end
 
 			end
-			puts
+			puts # Go to the next line for next row
 		end
  		
+ 		# Print markers for each stack location
  		@stacks.length.times do
  			print ('-' * @disks).center(11)
  		end
  		puts
+ 		
 	end
 
 	def get_input
@@ -61,7 +82,7 @@ class TowersOfHanoi
 			puts "Enter stack to move from and to separated by a comma (eg 1,3) or 'q' to quit: "
 			print "> "
 
-			input = gets.chomp.strip.split(',')
+			input = gets.strip.split(',')
 			exit if input[0] == 'q'
 
 			return input[0].to_i, input[1].to_i if valid?(input[0],input[1]) 
@@ -79,22 +100,17 @@ class TowersOfHanoi
 	end
 
 	def valid_source?(source)
-		if (1..3).include? source
-			puts "valid_source: #{!@stacks[ source-1 ].empty?}"
-			return !@stacks[ source-1 ].empty?
-		end
-
-		puts "valid_source: false"
+		# Check if source stack is in range, if so, make sure it's not empty
+		return !@stacks[ source-1 ].empty? if (1..3).include? source
 		return false
 	end
 
 	def valid_destination?(destination)
-		puts "valid_destination: #{(1..3).include? destination}"
 		return (1..3).include? destination
 	end
 
 	def top_disk_smaller?(source, destination)
-		puts "top_disk_smaller: #{@stacks[ destination-1 ].empty? || @stacks[ destination-1 ][-1] > @stacks[ source-1 ][-1] }"
+		# Make sure destination stack is either empty or has a bigger disk then the disk to put on top of it.
 		return @stacks[ destination-1 ].empty? || @stacks[ destination-1 ][-1] > @stacks[ source-1 ][-1] 
 	end
 
@@ -104,17 +120,17 @@ class TowersOfHanoi
 	end
 
 	def victory?
-		return true if @stacks[1] == full_stack|| @stacks[2] == full_stack
+		return true if @stacks[1] == full_stack || @stacks[2] == full_stack
 		return false
 	end
 
 	def play_again?
-		print "Wanna play again? (y/n): "
+		print "Want to play again? (y/n): "
 		choice = gets.strip.downcase
 
 		case choice
 		when 'y' then return true
-		when 'n' then exit
+		when 'n' then return false
 		else
 			puts "Invalid entry, exiting the game."
 			return false
@@ -122,6 +138,9 @@ class TowersOfHanoi
 	end
 end
 
-game = TowersOfHanoi.new(5)
+input = ARGV
+disks = ARGV[0].to_i unless ARGV.empty?
+
+game = TowersOfHanoi.new( disks ||= 3 )
 
 game.play
